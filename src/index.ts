cb790cdabf204879507c58b9af8e20ff50c7126d
Fischer <fischerrobson@gmail.com>
2021-07-01 20:46:25 -0300

    rota de post

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,12 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-    return res.json("{user:'amor'}");
+    return res.json({ user: 'amor' });
 });
+
+app.post("/", (req, res) => {
+    return res.send();
+})
 
 app.listen(3333, () => {
     console.log("Server is running");
