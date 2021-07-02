0f992c469e0948754aa1c23e5b7eb392c509a3c8
Fischer <fischerrobson@gmail.com>
2021-07-02 20:45:12 -0300

    database connection and migrations setup

---
+++ src/database/index.ts
@@ -0,0 +1,8 @@
+import { createConnection } from "typeorm";
+
+createConnection();
+
+//yarn typeorm migration:create -n NomeDaMigration -> criar migration
+
+//yarn typeorm migration:run -> executar migrations
+//yarn typeorm migration:revert -> reverte a ultima migration
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,8 @@
 import express from "express";
+import "reflect-metadata"
+import "./database";
 
 const app = express();
-
-app.get("/", (req, res) => {
-    return res.json({ user: 'amor' });
-});
-
-app.post("/", (req, res) => {
-    return res.send();
-})
 
 app.listen(3333, () => {
     console.log("Server is running");
