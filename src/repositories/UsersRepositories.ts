aed428e7424117c6062e4d739f37e670e74a00e7
Fischer <fischerrobson@gmail.com>
2021-07-06 00:11:50 -0300

    User repository

---
+++ src/repositories/UsersRepositories.ts
@@ -0,0 +1,9 @@
+import { EntityRepository, Repository } from "typeorm";
+import User from '../entities/User';
+
+@EntityRepository(User)
+class UsersRepostories extends Repository<User> {
+
+}
+
+export default UsersRepostories;
